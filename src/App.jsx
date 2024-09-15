b2daa71bb238b1413e9369d80332dfaf0a09f09a
ehilel <ehilel@g.jct.ac.il>
2024-09-15 20:23:55 +0300

    Adding design

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Weather from './components/Weather'
 
 const App = () => {
   return (
-    <div className="app">
+    <div className='container'>
+      <h1 className="header">מזג אוויר</h1>
+      <div className="app">
       {['Eilat', 'London', 'New York', 'Alaska'].map((city) => (
         <Weather key={city} city={city} />
       ))}
     </div>
+    </div>
+    
   );
 };
 
